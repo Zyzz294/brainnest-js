d0a60f716b7ad4180c0d6d51c57bb4cf6442b4c0
Zyzz294 <96903120+Zyzz294@users.noreply.github.com>
2022-09-10 20:41:49 +0600

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -20,12 +20,3 @@ function clr(){
 function del(){
     result.value=result.value.slice(0,-1)
 }
-
-document.onkeypress = function (e) {
-    keyPressed = String.fromCharCode(e.which);        
-    if($("#calc").length > 0){
-      $("#calc").append(keyPressed);
-    }else{
-      $("#calc").text(keyPressed);
-    }
-};
